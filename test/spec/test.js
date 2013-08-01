19c080a948d266c7a3997b0473bf9b1f7ab49d0a
unknown <renan.ferreira@D0000-016.mcom.com.br>
2013-08-01 08:35:26 -0300

    revision

--- test/spec/test.js
+++ test/spec/test.js
@@ -12,7 +12,7 @@
 
     describe('Testemunha Testes', function () {
         describe('construtor', function () {        	
-            it('devera gerar uma nova exception "Parâmetros não podem ser nulos!"', function () {            	
+            it('devera gerar uma nova exception "Parâmetros não podem ser nulos!', function () {            	
             	expect(function() {
         			var jose = new Testemunha();
             	}).to.not.throw(new Error('Parâmetros não podem ser nulos!'));
@@ -20,19 +20,19 @@
         });
 
         describe('validar teoria', function () {        	
-            it('devera retornar 1, 2 ou 3 caso o assassino, loca e a arma estejam incorretos!"', function () {            	
+            it('devera retornar 1, 2 ou 3 caso o assassino, loca e a arma estejam incorretos.', function () {            	
             	expect(testemunha.validarTeoria(3, 5, 1)).to.not.equal(0);
             });
 
-            it('devera retornar 1 caso o assassino esteja incorreto"', function () {            	
+            it('devera retornar 1 caso o assassino esteja incorreto', function () {            	
             	expect(testemunha.validarTeoria(3, 3, 5)).to.be.equal(1);
             });             
             
-            it('devera retornar 2 caso o local esteja errado!"', function () {            	
+            it('devera retornar 2 caso o local esteja errado.', function () {            	
             	expect(testemunha.validarTeoria(2, 5, 4)).to.be.equal(2);
             });           
 
-            it('devera retornar 3 caso a arma esteja errada!"', function () {            	
+            it('devera retornar 3 caso a arma esteja errada.', function () {            	
             	expect(testemunha.validarTeoria(2, 3, 5)).to.be.equal(3);
             });                                   
         });    	
@@ -40,13 +40,13 @@
 
     describe('Detetive Testes', function () {
 
-        it('teoria deve ser diferente caso a resposta seja diferente', function () {   
+        it('teoria deve ser diferente caso a resposta seja diferente.', function () {   
              var teoria = detetive.criarTeoria();
 
              expect(detetive.criarTeoriaBaseadoEmResposta(2)).to.not.eql(teoria);
         });
  
-        it('resposta deve ser igual a 0', function () {   
+        it('resposta deve ser igual a 0.', function () {   
             var resposta,
                 data = new Data(),
                 randomAssassino = data.assassinos[_.random(0 , data.assassinos.length - 1)],
